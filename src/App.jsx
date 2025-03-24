f70c8c30f33b27802baa10c29ec4fddc71ebdb5b
Sokratius <lipnaurobert403@mail.ru>
2025-03-24 10:00:55 +0500

    Created Menu and Main components (Not Completed)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Sidebar from "./components/Sidebar/Sidebar.jsx";
+import Main from './components/Main/Main.jsx';
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Sidebar />
+      <Main />
+    </>
   )
 }
 
